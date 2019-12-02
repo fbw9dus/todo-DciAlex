891fb19cf98f08357b6646ee524aa51a8b835882
DciAlex <alexgherman68@gmail.com>
2019-12-02 12:01:56 +0100

    update 0.1

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ input = document.querySelector('.text');
 btn = document.querySelector('.btn')
 body = document.querySelector('body')
 
-const inputHandlerFactory = (button,input)=> {
+const inputHandlerFactory = (button,input,x)=> {
     const addText = () =>{
        
         const h2 = document.createElement('h2');
-        h2.innerText  = input.value
+        h2.innerText  = input.value ;
         h2.style.backgroundColor = 'red'
+        
+        // x = document.createTextNode("X");
+       
         body.append(h2);
-
-
+        // h2.append(x);
     }
-    button.addEventListener('click',addText)
 }
-inputHandlerFactory(btn,input)
+inputHandlerFactory(btn,input);
+
+body.addEventListener('click',e =>{
+    if(e.target.tagName === 'H2'){
+        e.target.remove();
+    }
+});
+
+
+
